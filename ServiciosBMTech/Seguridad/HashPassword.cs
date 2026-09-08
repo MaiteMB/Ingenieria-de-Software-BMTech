8cf4ed12d1c262a9a0b1eaccc439336e134f8379
MaiteMB <122162891+MaiteMB@users.noreply.github.com>
2026-09-08 12:14:29 -0300

    Implementa hash con bcrypt para contraseñas

---
+++ ServiciosBMTech/Seguridad/HashPassword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiciosBMTech.Seguridad
+{
+    public class HashPassword
+    {
+        public string GenerarHash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, 12);
+        }
+
+        public bool VerificarPassword(string passwordIngresada, string hashAlmacenado)
+        {
+            return BCrypt.Net.BCrypt.Verify(passwordIngresada, hashAlmacenado);
+        }
+    }
+}
